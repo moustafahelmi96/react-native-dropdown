69f6c4924d39ae458394835d5d53477b367fa374
macbook pro <macbookpro@macbooks-MacBook-Pro.local>
2020-10-22 17:21:14 +0200

    android fixes

--- src/DropdownModal.js
+++ src/DropdownModal.js
@@ -1,14 +1,12 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, { useState, useRef } from 'react'
-import { Dimensions } from 'react-native'
-import Modal from 'react-native-modal'
-import Carousel from 'react-native-snap-carousel'
-import { useEffect } from 'react/cjs/react.development'
-import styled from 'styled-components'
-
-const screenWidth = Dimensions.get('window').width
-const screenHeight = Dimensions.get('window').height
-
+import React, { useState, useRef } from "react";
+import { Dimensions } from "react-native";
+import Modal from "react-native-modal";
+import Carousel from "react-native-snap-carousel";
+import { useEffect } from "react/cjs/react.development";
+import styled from "styled-components";
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
 const DropdownModal = ({
   items,
   showDropdown,
@@ -21,26 +19,22 @@ const DropdownModal = ({
   setSelected,
   onSelect,
 }) => {
-  const [selectedIndex, setSelectedIndex] = useState(0)
-  const carousel = useRef(null)
-
-  const onSnapToItem = selectedInd => {
-    setSelectedIndex(selectedInd)
-    setSelected(items[selectedInd])
-    onSelect(items[selectedInd])
-  }
-
-  const handleMarker = index => {
-    carousel.snapToItem(index, true)
-  }
-
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const carousel = useRef(null);
+  const onSnapToItem = (selectedInd) => {
+    setSelectedIndex(selectedInd);
+    setSelected(items[selectedInd]);
+    onSelect(items[selectedInd]);
+  };
+  const handleMarker = (index) => {
+    carousel.snapToItem(index, true);
+  };
   // useEffect(() => handleMarker(3), [])
-
   return (
     <Modal
       isVisible={showDropdown}
       onBackdropPress={() => setShowDropdown(false)}
-      style={{ justifyContent: 'flex-end', margin: 0 }}
+      style={{ justifyContent: "flex-end", margin: 0 }}
     >
       <ModalContainer>
         <ButtonContainer>
@@ -64,7 +58,7 @@ const DropdownModal = ({
               data={items}
               onSnapToItem={onSnapToItem}
               onLayout={() => {
-                carousel.current.snapToItem(selectedIndex, true)
+                carousel.current.snapToItem(selectedIndex, true);
               }}
               renderItem={({ item, index }) => (
                 <ModalItemContainer key={index} seperatorColor={seperatorColor}>
@@ -78,55 +72,48 @@ const DropdownModal = ({
         </Container>
       </ModalContainer>
     </Modal>
-  )
-}
-
-export default DropdownModal
-
+  );
+};
+export default DropdownModal;
 const ModalContainer = styled.View`
   width: ${screenWidth}px;
   align-self: center;
   height: ${screenHeight * 0.3}px;
-`
-
+`;
 const Container = styled.View`
   background-color: ${({ backgroundColor }) =>
-    backgroundColor || 'rgb(208,212,218)'};
-  height: 35%;
-`
-
+    backgroundColor || "rgb(208,212,218)"};
+  height: ${screenHeight * 0.3}px;
+`;
 const ModalItemContainer = styled.View`
   width: ${screenWidth * 0.9}px;
   height: ${screenHeight * 0.05}px;
   align-self: center;
   justify-content: center;
-`
-
+`;
 const ModalItemText = styled.Text`
   text-align: center;
   font-size: ${20}px;
-  color: ${({ itemsColor }) => itemsColor || 'black'};
-`
-
+  color: ${({ itemsColor }) => itemsColor || "black"};
+`;
 const ButtonContainer = styled.View`
   width: ${screenWidth}px;
   height: ${screenHeight * 0.05}px;
   justify-content: center;
   background-color: rgb(240, 241, 242);
-`
-
+`;
 const Button = styled.TouchableOpacity`
   width: ${screenWidth * 0.2}px;
   justify-content: center;
-`
-
+`;
 const ButtonText = styled.Text`
   text-align: center;
   font-size: ${20}px;
-  color: ${({ buttonColor }) => buttonColor || 'black'};
-`
-
+  color: ${({ buttonColor }) => buttonColor || "black"};
+`;
 const OuterModalContainer = styled.SafeAreaView`
   background-color: ${({ backgroundColor }) =>
-    backgroundColor || 'rgb(208,212,218)'};
-`
+    backgroundColor || "rgb(208,212,218)"};
+  background-color: red;
+  height: ${screenHeight * 0.3}px;
+`;
